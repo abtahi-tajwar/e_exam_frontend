596c98b8138e51ce6748d536a34ef475c239671d
abtahi_tajwar <abtahitajwar@gmail.com>
2021-09-12 12:57:09 +0600

    Exam page added

---
+++ assets/js/utils.js
@@ -0,0 +1,17 @@
+function startTimer(duration, display, callback) {
+    var timer = duration, minutes, seconds;
+    var interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            callback();
+            clearInterval(interval)
+        }
+    }, 1000);
+}

c7e5abadeea52e8021b86574208f1de97f6f649a
abtahi_tajwar <abtahitajwar@gmail.com>
2021-10-12 22:00:01 +0600

    Navbar sticky top added

--- assets/js/style.js
+++ assets/js/style.js
@@ -32,7 +32,21 @@ window.addEventListener('scroll', () => {
         }
     })
     
-    
+    // Sticky bootstrap navbar
+
+    if (window.scrollY > 200) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        document.getElementById('navbar_top').classList.add('bg-dark');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+        document.getElementById('navbar_top').classList.remove('bg-dark');
+            // remove padding top from body
+        document.body.style.paddingTop = '0';
+    } 
+ 
 })
 
 ////////////////////
